2352ef57c71f89dd5e46522d586c89989ccc19e4
Vinicius Troyack <troyackvinicius@gmail.com>
2024-03-13 20:45:16 -0300

    Criando header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
 import styles from "./page.module.css";
+import Image from "next/image";
+
+import heroImg from "../../public/assets/hero.png"
 
 export default function Home() {
+
   return (
-    <main className={styles.main}>
-      <h1>Minhas tarefas</h1>
-    </main>
+      <main className={styles.container}>
+        <div className={styles.logoContent}>
+          <Image 
+            className={styles.hero}
+            alt="Logo Tarefas+"
+            src={heroImg}
+            priority/>
+        </div>
+        <h1 className={styles.title}>
+          Sistema feito para você organizar <br/>
+          seus estudos e tarefas
+        </h1>
+
+        <div className={styles.infoContent}>
+          <section className={styles.box}>
+            <span>+12 posts</span>
+          </section>
+          <section className={styles.box}>
+            <span>+90 comentários</span>
+          </section>
+        </div>
+      </main>
   );
 }
