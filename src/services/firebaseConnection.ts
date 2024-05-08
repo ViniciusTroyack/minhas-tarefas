bf87cffe191e134ac79b9782126e7a26fc1f121c
Vinicius Troyack <troyackvinicius@gmail.com>
2024-05-08 16:28:59 -0300

    Fix: .env

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2Ju_HGh8VlhX4_IUfiHXqIf8ECiG0-Kc",
-  authDomain: "minhas-tarefas-b4a02.firebaseapp.com",
-  projectId: "minhas-tarefas-b4a02",
-  storageBucket: "minhas-tarefas-b4a02.appspot.com",
-  messagingSenderId: "405861611973",
-  appId: "1:405861611973:web:e349c7277452f87e0673ac",
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
